aee28671036b14ff867a8db3a7416b33c5deb2c8
Arol Wright <arol@globcal.net>
2025-02-18 19:58:04 -0400

    Removing stuff I don't need

--- .eleventy.js
+++ .eleventy.js
@@ -14,18 +14,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("media");
     // Add main JS
     eleventyConfig.addPassthroughCopy("main.js");
-    // Download button shortcode
-    eleventyConfig.addShortcode("downloadBtn", function (platform, url) {
-        var html = '';
-        if (platform === 'chrome') {
-            html = `<a href="${url}" target="_blank" class="btn-container"><img src="/media/chrome-button.png" alt="Download on Chrome Web Store" /></a>`;
-        } else if (platform === 'firefox') {
-            html = `<a href="${url}" target="_blank" class="btn-container"><img src="/media/firefox-button.png" alt="Download for Firefox" /></a>`;
-        } else if (platform === 'edge') {
-            html = `<a href="${url}" target="_blank" class="btn-container"><img src="/media/microsoft-button.png" alt="Download for Microsoft Edge" /></a>`;
-        }
-        return html;
-    });
     // Profile picture shortcode
     eleventyConfig.addShortcode("photo", function (url) {
         return `<img alt="Photo of Arol" class="profile-photo" src="https://www.gravatar.com/avatar/7e45f50667c4ed988aee238bab4fb7a03449ad993c42e230317afaa1417c6140?s=500">`
@@ -33,21 +21,6 @@ module.exports = function (eleventyConfig) {
     // Redirect shortcode
     eleventyConfig.addShortcode("redirect", function (url) {
         return `<p>You are being redirected...</p><meta http-equiv="refresh" content="0; url=${url}" />`
-    });
-    // Letterboxd shortcode
-    eleventyConfig.addShortcode("letterboxd", async function (url) {
-        let html = '';
-        let feed = await parser.parseURL('https://letterboxd.com/corbindavenport/rss/');
-        // console.log('Parsed feed:', feed);
-        for (let i = 0; i < Math.min(feed.items.length, 5); i++) {
-            let review = feed.items[i];
-            let snippet = review.contentSnippet;
-            if (snippet.length > 300) {
-                snippet = snippet.substring(0, 300) + '...';
-            }
-            html += '<p><a href="' + review.link + '" target="_blank" rel="nofollow">' + review.title + '</a><br /><i>' + snippet + '</i></p>';
-        }
-        return html;
     });
     // Set nofollow, noreferrer, noopener, and target blank attributes for all external links
 	eleventyConfig.addTransform("update-links", async function (content) {
